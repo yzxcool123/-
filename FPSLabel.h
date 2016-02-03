c6b3bb676a25dedf9bf11df1219d6abe3e8a6de8
yezhongxiang <756374893@qq.com>
2016-02-03 12:37:56 +0800

    initialize

---
+++ FPSLabel.h
@@ -0,0 +1,13 @@
+//
+//  FPSLabel.h
+//  OnFire
+//
+//  Created by YeZhongxiang on 16/1/28.
+//  Copyright © 2016年 OnFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPSLabel : UILabel
+
+@end
